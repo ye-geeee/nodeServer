6e97d7c29161cf7aff53b05006b6a99d40c4a7e1
Yeji Kang <60929159+ye-geeee@users.noreply.github.com>
2020-05-22 23:32:49 +0900

    Add Emmail Router (#4)

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var exphbs  = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var uploadRouter = require('./routes/upload');
+var emailRouter = require('./routes/email');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('handlebars', exphbs());
 
 app.use(cors());
 app.use(logger('dev'));
@@ -25,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/upload', uploadRouter);
 app.use('/users', usersRouter);
+app.use('/email', emailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

7d7625d09d63bfe3105853655f5443474d0227b5
ye-geeee <yeaji9683@gmail.com>
2020-06-30 06:50:04 +0900

    Define the relation between object

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var uploadRouter = require('./routes/upload');
 var emailRouter = require('./routes/email');
+var sequelize = require('./models').sequelize;
 
 var app = express();
+sequelize.sync();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
